2c753d593156650b0de2481d17a4e615cad57891
chenivan0214 <chenjiahau@snailhouse.com.tw>
2016-08-23 17:26:37 +0800

    Change: webpack init again

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,33 +1,31 @@
 "use strict";
 
-var modWebpack = require('webpack');
-var objHost = {
-        ip: "0.0.0.0",
-        port: "8080"
-    };
+var webpack = require('webpack'),
+    port = 8000;
 
 module.exports = {
     entry: [
-        'webpack-dev-server/client?http://localhost:' + objHost.port,
-        'webpack/hot/dev-server',
-        './app/main.js'
+        "webpack/hot/dev-server",
+        "./app/main.js"
     ],
     output: {
         path: "./build",
         filename: "./js/bundle.js"
     },
-    loaders: [{
-        test: /\.js$/,
-        loaders: [],
-        include: "./app"
-
-    }],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel'
+            }
+        ]
+    },
     devServer: {
+        hot: true,
         inline: true,
-        host: objHost.ip,
-        port: objHost.port
+        port: port
     },
     plugins: [
-        new modWebpack.NoErrorsPlugin()
+        new webpack.HotModuleReplacementPlugin()
     ]
 };
