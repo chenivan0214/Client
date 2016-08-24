009b6036685fde5119c48bf370f83a362167a3cb
chenivan0214 <chenjiahau@snailhouse.com.tw>
2016-08-25 07:21:00 +0800

    Change: webpack add babel-preset-es2015

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
                 loader: 'babel',
                 exclude: /node_modules/,
                 query: {
-                    presets: ['react']
+                    presets: ['es2015', 'react']
                 }
             }
         ]
