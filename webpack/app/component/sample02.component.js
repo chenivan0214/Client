7b287caf71a7aec804e96392d5fa19ac7f0c8ac8
chenivan0214 <chenjiahau@snailhouse.com.tw>
2016-08-28 07:50:34 +0800

    Change: sample02 add more operate like reduce、clear @webpack

--- webpack/app/component/sample02.component.js
+++ webpack/app/component/sample02.component.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const List = (props) => (
         <div>
-            Click count:{props.clickCount}
             <ul>
                 {
                     props.data.map((item) => (
@@ -17,52 +16,68 @@ class Sample extends React.Component {
     constructor(props) {
         super(props);
 
-        this.onClick = this.onClick.bind(this);
+        this.onClickAdd = this.onClickAdd.bind(this);
+        this.onClickRedure = this.onClickRedure.bind(this);
+        this.onClickClear = this.onClickClear.bind(this);
 
         this.state = {
-            isClick: false,
-            opText: "Add",
             clickCount: 0
         };
     }
 
-    onClick(e) {
-        if (this.state.isClick) {
-            this.setState({
-                isClick: false,
-                opText: "Add",
-                clickCount: ++this.state.clickCount
-            });
-        } else {
-            this.setState({
-                isClick: true,
-                opText: "Reduce",
-                clickCount: ++this.state.clickCount
-            });
+    onClickAdd(e) {
+        this.setState({
+            clickCount: ++this.state.clickCount
+        });
+
+
+        var nextId = this.props.data.length + 1,
+            nextText = "No." + nextId,
+            nextData = { id: nextId, text: nextText };
+
+        this.props.data.push(nextData);
+    }
+
+    onClickRedure(e) {
+        this.setState({
+            clickCount: ++this.state.clickCount
+        });
+
+        if (this.state.clickCount !== 0) {
+            this.props.data.pop();
         }
     }
 
+    onClickClear() {
+        this.props.data.length = 0;
+        this.setState({
+            clickCount: ++this.state.clickCount
+        });
+    }
+
     render() {
-        if (this.state.isClick) {
-            var nextId = this.props.data.length + 1,
-                nextText = "No." + nextId,
-                nextData = { id: nextId, text: nextText };
-
-            this.props.data.push(nextData);
-        } else {
-            if (this.state.clickCount !== 0
-                && this.state.clickCount % 3 === 0) {
-                this.props.data.pop();
-            }
-        }
-
         return (
             <div>
-                <List data={this.props.data} clickCount={this.state.clickCount}/>
-                <button type="button" onClick={this.onClick}>{this.state.opText}</button>
+                <div>
+                    Click count:{this.state.clickCount}
+                </div>
+                <div>
+                    <button type="button" onClick={this.onClickAdd}>Add</button>
+                    <button type="button" onClick={this.onClickRedure}>Reduce</button>
+                    <button type="button" onClick={this.onClickClear}>Clear</button>
+                </div>
+                <List data={this.props.data}/>
             </div>
         )
     }
 };
+
+Sample.propTypes = {
+    data: React.PropTypes.array,
+}
+
+Sample.defaultProps = {
+    data: [],
+}
 
 module.exports = Sample;
--- webpack/app/main.js
+++ webpack/app/main.js
@@ -9,13 +9,5 @@ import ReactDOM from 'react-dom';
 //import Sample01 from './component/sample01.component.js';
 //ReactDOM.render(<Sample01 data={data}/>, document.getElementById('app'));
 
-var aryList = [
-        {id: 1, text: "No.1"},
-        {id: 2, text: "No.2"},
-        {id: 3, text: "No.3"},
-        {id: 4, text: "No.4"},
-        {id: 5, text: "No.5"}
-    ];
-
 import Sample02 from './component/sample02.component.js';
-ReactDOM.render(<Sample02 data={aryList}/>, document.getElementById('app'));
+ReactDOM.render(<Sample02/>, document.getElementById('app'));
