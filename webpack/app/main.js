48da5526960deff52d4127f365877327cecd213f
chenivan0214 <chenjiahau@snailhouse.com.tw>
2016-08-28 10:20:55 +0800

    Change: wrote comment @webpack

--- webpack/app/main.js
+++ webpack/app/main.js
@@ -3,6 +3,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/**
+ ** Sample01 on native js 
+ ** - component
+ ** - state & props
+ **/
 //var data = {
 //        firstName: "Ivan",
 //        lastName: "Chen"
@@ -11,11 +16,17 @@ import ReactDOM from 'react-dom';
 //import Sample01 from './component/sample01.component.js';
 //ReactDOM.render(<Sample01 data={data}/>, document.getElementById('app'));
 
-import Sample02 from './component/sample02.component.js';
-ReactDOM.render(<Sample02/>, document.getElementById('app'));
+/**
+ ** Sample02 on es2015
+ ** - component
+ ** - state & props
+ ** - life cycle   
+ **/
+//import Sample02 from './component/sample02.component.js';
+//ReactDOM.render(<Sample02/>, document.getElementById('app'));
 
 //trigger other life cycle on sample02
-setTimeout(() => {
-    ReactDOM.render(<Sample02 otherProps={'nothing'}/>, document.getElementById('app'));
-    ReactDOM.unmountComponentAtNode(document.getElementById('app'));
-}, 10000);
+//setTimeout(() => {
+//    ReactDOM.render(<Sample02 otherProps={'nothing'}/>, document.getElementById('app'));
+//    ReactDOM.unmountComponentAtNode(document.getElementById('app'));
+//}, 10000);
