940df01b79b5b1c4962d36c168ef2a5c63116f32
chenivan0214 <chenjiahau@snailhouse.com.tw>
2016-08-29 21:33:25 +0800

    Change: add flux sample @webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: {
         basic_react_es5: ["webpack/hot/dev-server", "./app/entry/basic_react_es5.entry.js"],
         basic_react: ["webpack/hot/dev-server", "./app/entry/basic_react.entry.js"],
-        react_router: ["webpack/hot/dev-server", "./app/entry/react_router.entry.js"]
+        react_router: ["webpack/hot/dev-server", "./app/entry/react_router.entry.js"],
+        flux: ["webpack/hot/dev-server", "./app/entry/flux.entry.js"]
     },
     output: {
         path: "./build",
