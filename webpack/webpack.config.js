997d8515e063ef1f72b73d2a914746045ce4e5b8
chenivan0214 <chenjiahau@snailhouse.com.tw>
2016-08-30 21:06:58 +0800

    Change: webpack loader use short course

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|jpeg|gif|woff)$/,
-                loader: 'url-loader'
+                loader: 'url'
             },
             {
                 test: /\.json$/,
